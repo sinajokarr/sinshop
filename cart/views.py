272e5e6aa69746733fb0fe43d5c542d44c512bed
Sina Jokar <cnajokar11@yahoo.com>
2026-03-21 14:52:44 +0300

    view cart is updating

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
 
-# Create your views here.
+class CartViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.DestroyModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [AllowAny] 
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    serializer_class = CartItemSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
