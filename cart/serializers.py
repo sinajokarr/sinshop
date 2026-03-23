52ce6df582a8a892df9332e1cb639416093c9ebf
Sina Jokar <cnajokar11@yahoo.com>
2026-03-23 11:58:06 +0300

    update serializer on the cart app

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
-
+from shop.models import Product
 class CartItemSerializer(serializers.ModelSerializer):
     total_price = serializers.ReadOnlyField()
     product_name = serializers.CharField(source='product.name', read_only=True)
@@ -20,3 +20,31 @@ class CartSerializer(serializers.ModelSerializer):
 
     def get_grand_total(self, cart):
         return sum([item.total_price for item in cart.items.all()])
+    
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("No product with this ID was found!")
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context["cart_id"]
+        product_id = self.validated_data["product_id"]
+        quantity = self.validated_data.get("quantity", 1)
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            cart_item.save() 
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+
+        return self.instance
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product_id', 'quantity']
