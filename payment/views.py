f10b8a9bf84178d8eb785471d98f3dc6321c1f49
Sina Jokar <cnajokar11@yahoo.com>
2026-04-01 16:20:11 +0300

    updaate app payment

--- payment/views.py
+++ payment/views.py
@@ -6,3 +6,10 @@ from orders.models import Order
 from .models import Payment
 import requests
 
+
+class PaymantStarView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post (self,request,pk):
+        order = get_object_or_404(Order ,id= pk ,user= request.user )
+        return Response({"message": "Ready to connect to bank..."})
